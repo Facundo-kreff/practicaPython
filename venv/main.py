6d7ad69d48827ec09a94ae043f25b31892918327
Facundo Kreff <77506852+Facundo-kreff@users.noreply.github.com>
2021-12-02 22:08:09 -0300

    Listo desde opcion 1 a 6

--- venv/main.py
+++ venv/main.py
@@ -8,47 +8,101 @@ import random
 # rojo, 1: mal estacionamiento, etc.) y finalmente el importe a pagar por la misma (un número en coma flotante).
 
 class Multa:
-    # Generador patente
-    def Patente():
-        a = random.randrange(1, 3)
-        if a == 1:
-            letras = random.choice(("ABCDEFGHYJKLMOPQRSTUVWXYZ")) + random.choice(
-                ("ABCDEFGHYJKLMOPQRSTUVWXYZ")) + random.choice(("ABCDEFGHYJKLMOPQRSTUVWXYZ"))
-            numeros = str(random.randrange(10)) + str(random.randrange(10)) + str(random.randrange(10))
-            patente = letras + str(numeros)
-            return patente
-        else:
-            letras1 = random.choice(("ABCDEFGHYJKLMOPQRSTUVWXYZ")) + random.choice(("ABCDEFGHYJKLMOPQRSTUVWXYZ"))
-            numeros = str(random.randrange(10)) + str(random.randrange(10)) + str(random.randrange(10))
-            letras2 = random.choice(("ABCDEFGHYJKLMOPQRSTUVWXYZ")) + random.choice(("ABCDEFGHYJKLMOPQRSTUVWXYZ"))
-            patente = letras1 + str(numeros) + letras2
-            return patente
-
-    def __init__(self, numIden=random.randint(1,400), patente=Patente(), tipoMulta=random.randint(0, 24), montoPagar=random.randint(500,4000)):
+    def __init__(self, numIden, patente, tipoMulta, montoPagar):
         self.numero=numIden
         self.patente=patente
         self.tipoMulta=tipoMulta
         self.monto=montoPagar
 
+# Desarrollo de la opcion 1
+# Generador patente
+def Patente():
+    a = random.randrange(1, 3)
+    if a == 1:
+        letras = random.choice(("ABCDEFGHYJKLMOPQRSTUVWXYZ")) + random.choice(
+            ("ABCDEFGHYJKLMOPQRSTUVWXYZ")) + random.choice(("ABCDEFGHYJKLMOPQRSTUVWXYZ"))
+        numeros = str(random.randrange(10)) + str(random.randrange(10)) + str(random.randrange(10))
+        patente = letras + str(numeros)
+        return patente
+    else:
+        letras1 = random.choice(("ABCDEFGHYJKLMOPQRSTUVWXYZ")) + random.choice(("ABCDEFGHYJKLMOPQRSTUVWXYZ"))
+        numeros = str(random.randrange(10)) + str(random.randrange(10)) + str(random.randrange(10))
+        letras2 = random.choice(("ABCDEFGHYJKLMOPQRSTUVWXYZ")) + random.choice(("ABCDEFGHYJKLMOPQRSTUVWXYZ"))
+        patente = letras1 + str(numeros) + letras2
+        return patente
 
-#Desarrollo de la opcion 1
 def Opcion_1():
     registro = []
     cantMultas = int(input("Cantidad de multas a ingresar: "))
     for i in range(cantMultas):
-        registro.append(Multa())
-        print('\nNuevo ingreso')
-        print('Numero: ' + str(registro[i].numero) + '\nPatente: ' + str(registro[i].patente))
-        print('Tipo Multa: ' + str(registro[i].tipoMulta) + '\nMonto: ' + str(registro[i].monto))
+        iden = random.randint(1,400)
+        pat = Patente()
+        tipo = random.randint(0, 24)
+        monto = random.randint(500,4000)
+        registro.append(Multa(iden, pat, tipo, monto))
+
+        if len(registro) > 1:
+            for j in range(len(registro)):
+                if registro[i].numero < registro[j].numero:
+                    aux=registro[i]
+                    registro[i]=registro[j]
+                    registro[j]=aux
+    print("\033[1;37m""\nCarga de datos exitosa!!")
     return registro
 
-#Desarrollo de la opcion 2
-def Opcion_2(ar):
+# Desarrollo de la opcion 2 y 4
+def Mostrar(ar):
     if type(ar) == str:
         print("\033[0;31"+"aAun no se a cargado los datos"+"\033[0;m")
     else:
         for i in range(len(ar)):
             print("   N° identificacion: " ,ar[i].numero,"   Patente: " ,ar[i].patente,"   Tipo: " ,ar[i].tipoMulta,"   Monto: " ,ar[i].monto)
+
+# Desarrollo opcion 3
+def Opcion_3(ar):
+    if type(ar) == str:
+        print("\033[0;31"+"aAun no se a cargado los datos"+"\033[0;m")
+        return
+    else:
+        suma=0
+        for i in range(len(ar)):
+            suma+=ar[i].monto
+        promedio= suma/len(ar)
+        nuevoAr=[]
+        for i in range(len(ar)):
+            if ar[i].monto > promedio:
+                nuevoAr.append(ar[i])
+        print("\033[1;37m""\nCarga de datos exitosa!!")
+        return nuevoAr
+
+# Desarrollo opcion 5
+def Busqueda(ar):
+    if type(ar) == str:
+        print("\033[0;31"+"aAun no se a cargado los datos"+"\033[0;m")
+        return
+    else:
+        aux=int(input("\033[4;m""\nIngrese el N° identificacion que desea buscar: "))
+        for i in range(len(ar)):
+            if aux == ar[i].numero:
+                ar[i].monto*=1.1
+                print("\nSe le aplico el 10% de aumento")
+                return
+        print("\033[1;37m""\nNo se encontro ninguna coincidencia!")
+
+#Desarrollo de la opcion 6
+def Opcion_6(ar):
+    if type(ar) == str:
+        print("\033[0;31" + "aAun no se a cargado los datos" + "\033[0;m")
+        return
+    aux = 0
+    suma = 0
+    while aux > 24 or aux <= 0:
+        aux=int(input('\033[4;33m\nTipo de multa que busca:'+'\033[0,m')+' ')
+    for i in range(len(ar)):
+        if aux == ar[i].tipoMulta:
+            suma+=ar[i].monto
+    print("\nMonto total de la multa de tipo ", aux," es $", suma)
+
 
 # 1)Desarrollar un programa completo con menú de opciones que permita cumplir los requerimientos que se indican
 # a continuación. [máximo: 3 puntos – 12% del total]
@@ -56,6 +110,7 @@ def Opcion_2(ar):
 def Menu():
     i = 1
     arreglo = ""
+    arregloMayorProm=""
     # Ciclo While para generar menu
     while (i > 0 or i < 10):
         # Mostrar en pantalla menu
@@ -80,17 +135,17 @@ def Menu():
             arreglo = Opcion_1()
         # Opcion 2
         elif i == 2:
-            Opcion_2(arreglo)
+            Mostrar(arreglo)
         # Opcion 3
         elif i == 3:
-            print(3)
+            arregloMayorProm=Opcion_3(arreglo)
         # Opcion 4
         elif i == 4:
-            print(4)
+            Mostrar(arregloMayorProm)
         elif i == 5:
-            print(5)
+            Busqueda(arregloMayorProm)
         elif i == 6:
-            print(6)
+            Opcion_6(arreglo)
         elif i == 7:
             print(7)
         elif i == 8:
